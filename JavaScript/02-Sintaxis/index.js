9f6d9c37633125a7e4466a42d6a44e210a38a0bb
David Morales Garcia <davidmorales.gs@gmail.com>
2022-07-18 17:50:43 +0200

    Ejercicio 2

---
+++ JavaScript/02-Sintaxis/index.js
@@ -0,0 +1,26 @@
+/* 
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const array = [
+	'David',
+	35,
+	true,
+	new Date(1986, 8, 16),
+	{
+		titulo: 'El libro negro del Programador',
+		autor: 'Rafael Gomez Blanes',
+		fecha: 2017,
+		url: 'https://www.amazon.es/Libro-Negro-del-Programador-desarrollando/dp/1496153359/ref=sr_1_1?hvadid=80401819906729&hvbmt=bp&hvdev=c&hvqmt=p&keywords=libro+negro+programador&qid=1658158927&sr=8-1',
+	},
+]
